18a7cf18c342f902d74f7dd42c2abafc62545b33
pavanaar <pavanagowda2018@gmail.com>
2024-06-18 12:36:34 +0530

    Jenkins

---
+++ test/pageobjects/flipkart.js
@@ -0,0 +1,15 @@
+class shop{
+    get search(){
+        return $("//input[@class='Pke_EE']")
+    }
+    async run(){
+        await this.search.setValue("mobiles")
+    }
+    async dfsag(){
+        await this.search.setValue("mobiles")
+    }
+    async zcsasb(){
+        await this.search.setValue("mobiles")
+    }
+}
+export default new shop()
