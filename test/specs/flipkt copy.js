6c2de1061b94687989cfad03e7ba9af09c5292c8
pavanaar <pavanagowda2018@gmail.com>
2024-06-18 13:07:18 +0530

    try

---
+++ test/specs/flipkt copy.js
@@ -0,0 +1,7 @@
+import shop from "../pageobjects/flipkart.js"
+describe('search',async()=>{
+    it('',async()=>{
+        await browser.url("https://www.flipkart.com/")
+        await shop.run()
+    })
+})
